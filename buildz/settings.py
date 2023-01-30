2360933df7a567eff8fe5a3ce01dadfa9623df3d
Keder Nath mallick <53966650+keder-code-hash@users.noreply.github.com>
2023-01-30 11:44:26 +0530

    changed

--- buildz/settings.py
+++ buildz/settings.py
@@ -68,6 +68,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'chat.queryauthmiddleware.QueryAuthMiddleware'
 ]
 
 ROOT_URLCONF = 'buildz.urls'
---
+++ chat/queryauthmiddleware.py
@@ -0,0 +1,27 @@
+from channels.db import database_sync_to_async
+from channels.middleware import BaseMiddleware
+from authentication.models import User
+
+@database_sync_to_async
+def get_user(user_id):
+    try:
+        return User.objects.get(id=user_id)
+    except User.DoesNotExist:
+        return User.objects.get(id=6)
+
+class QueryAuthMiddleware(BaseMiddleware):
+    """
+    Custom middleware (insecure) that takes user IDs from the query string.
+    """
+
+    def __init__(self, app):
+        # Store the ASGI application we were passed
+        self.app = app
+
+    async def __call__(self, scope, receive, send):
+        # Look up user from query string (you should also do things like
+        # checking if it is a valid user ID, or if scope["user"] is already
+        # populated).
+        scope['user'] = await get_user(int(scope["query_string"]))
+
+        return await self.app(scope, receive, send)
